078bc466f8f1fb3f49f3bb7a97bfb68c5e64e695
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-04-21 16:07:48 -0400

    Restart containers after updating container IPs in /etc/hosts

--- dockerfiles/solr/run.sh
+++ dockerfiles/solr/run.sh
@@ -3,11 +3,12 @@
 dns=`ip addr show eth0 | grep inet | grep eth0 | awk '{print $2}' | cut -d "/" -f -1`
 docker build -t adsabs/solr .
 for id in 1 2; do
-  if [ $id = 1 ]; then
-    docker run -d -p 8193:8193 --name solr$id --dns $dns adsabs/solr
-  else
-    docker run -d -p 8193:8194 --name solr$id --dns $dns adsabs/solr
-  fi
+  docker run -d -p $((8982+id)):8983 --name solr$id --dns $dns adsabs/solr
 done
 
+#Assign container ips to host's /etc/hosts, then restart
 sudo .././set_hosts.sh
+for id in 1 2; do
+  docker stop solr$id
+  docker start solr$id
+done
--- dockerfiles/zookeeper/run.sh
+++ dockerfiles/zookeeper/run.sh
@@ -2,16 +2,10 @@
 
 #Target: adsabs-vagrant (pure devel environment)
 
-# p=2888
 dns=`ip addr show eth0 | grep inet | grep eth0 | awk '{print $2}' | cut -d "/" -f -1`
 for ZK_ID in 1 2 3; do
 
   echo $ZK_ID > myid
-  # p1=$((p+1))
-  # p2=$((p1+1000))
-
-  # sed -i "s/$p/$p1/g" Dockerfile
-  # sed -i "s/$((p+1000))/$p2/g" Dockerfile
 
   docker build -t adsabs/zookeeper$ZK_ID .
   if [ $ZK_ID = 1 ]; then
@@ -19,11 +13,12 @@ for ZK_ID in 1 2 3; do
   else
     docker run -d --name zookeeper$ZK_ID --dns $dns adsabs/zookeeper$ZK_ID
   fi
-
-  # sed -i "s/$p1/$p/g" Dockerfile
-  # sed -i "s/$p2/$((p+1000))/g" Dockerfile
   rm myid
-  # p=$p1
 done
 
+#Assign container ips to host's /etc/hosts, then restart
 sudo .././set_hosts.sh
+for ZK_ID in 1 2 3; do
+  docker stop zookeeper$ZK_ID
+  docker start zookeeper$ZK_ID
+done
