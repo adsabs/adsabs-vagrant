ed1dd1fd4a1cb57afd36d1c94b343762fe053c36
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-06-30 20:29:42 +0000

    Zookeeper ensemble on same host must wait for that host's /etc/hosts to be updated before starting the main process; Quickfix with sleep 5.

--- dockerfiles/zookeeper/run.sh
+++ dockerfiles/zookeeper/run.sh
@@ -8,18 +8,13 @@ for ZK_ID in 1 2 3; do
   echo $ZK_ID > myid
 
   docker build -t adsabs/zookeeper$ZK_ID .
-  if [ $ZK_ID = 1 ]; then
-    docker run -d --name zookeeper$ZK_ID -p 2181:2181 --dns $dns adsabs/zookeeper$ZK_ID
-  else
-    docker run -d --name zookeeper$ZK_ID --dns $dns adsabs/zookeeper$ZK_ID
-  fi
-  docker start zookeeper$ZK_ID
+  docker run -d --name zookeeper$ZK_ID --dns $dns --hostname zookeeper$ZK_ID adsabs/zookeeper$ZK_ID
+  docker stop zookeeper$ZK_ID
+
   rm myid
 done
 
-#Assign container ips to host's /etc/hosts, then restart
-sudo .././set_hosts.sh
 for ZK_ID in 1 2 3; do
-  docker stop zookeeper$ZK_ID
   docker start zookeeper$ZK_ID
 done
+sudo .././set_hosts.sh
---
+++ dockerfiles/zookeeper/zk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 5
+/opt/zookeeper-3.4.6/bin/zkServer.sh start
