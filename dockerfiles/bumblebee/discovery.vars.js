14c74bbb0caeb3cfc59c58ced6b94920236ca0ae
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-08-19 09:27:06 -0400

    Update bumblebee docker workflow

---
+++ dockerfiles/bumblebee/discovery.vars.js
@@ -0,0 +1,11 @@
+define([], function() {
+  "use strict";
+
+  // if you are not running from /, set the absolute path
+  // eg root: 'http://localhost:8000/test/foo/bar'
+  return {
+    root: '/',
+    debug: false
+  }
+});
+
