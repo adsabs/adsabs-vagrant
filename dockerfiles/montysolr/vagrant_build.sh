d78917220276aeed68a849b12b2c52dd0122468b
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-09-12 14:12:15 +0000

    WIP

--- dockerfiles/montysolr/vagrant_build.sh
+++ dockerfiles/montysolr/vagrant_build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 dns=`ip addr show eth0 | grep inet | grep eth0 | awk '{print $2}' | cut -d "/" -f -1`
-docker build -t adsabs/montysolr .
-for id in 3 4; do
-  docker run -d -p $((8982+id)):8983 --name montysolr$id --dns $dns adsabs/montysolr
+
+if [ ! -e "Dockerfile.origin" ]; then
+mv Dockerfile Dockerfile.origin
+fi
+
+for id in 1 2 3 4; do
+  shard=$((id%2+1))
+  sed "s/1\/*/$shard\/*/g" Dockerfile.vagrantbuild > Dockerfile
+  sed -i "s/H=\"\"/H=montysolr$id/g" run.sh
+  sed -i "s/shardId=\"\"/shardId=$shard/g" run.sh
+
+  docker build -t adsabs/montysolr$id .
+  docker run -d -p $((8982+id)):8983 --name montysolr$id --dns $dns adsabs/montysolr$id
   docker start montysolr$id
 done
