76c3fd56d19c54dbd68c14d4b70e93b3488bac08
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-11-14 13:54:52 -0500

    Add citations-helper-service deploy

---
+++ dockerfiles/citations/local_config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'this should be changed'
+
+SOLR_PATH = 'http://adsws.elasticbeanstalk.com/v1/search'
+TVRH_SOLR_PATH = 'http://adsws.elasticbeanstalk.com/v1/tvrh'
+
+#This section configures this application to act as a client, for example to query solr via adsws
+CLIENT = {
+  'TOKEN': ''
+}
