8391c1df5e5cc607ef2b7f2fa94f472b7a95b21b
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-08-07 19:52:45 +0000

    ecs/user_data: update with correct IP allocations

--- aws/user-data/ecs/user_data.sh
+++ aws/user-data/ecs/user_data.sh
@@ -3,25 +3,25 @@
 # Every instance in a cluster should have consul-agent, registrator, and logstash-forwarder 
 # running on it.
 
-IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-BRIDGE_IP="$(ip ro | awk '/^default/{print $3}')"
+IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n'`
+BRIDGE_IP="$(ip ro | awk '/docker0/{print $9}')"
 service docker start
 
 docker run -d \
-    --name consul \
-    -h $HOSTNAME \  
+    --name consul  \
+    -h $HOSTNAME \
     -p $IP:8300:8300 \
     -p $IP:8301:8301 \
-    -p $IP:8301:8301/udp \       
+    -p $IP:8301:8301/udp \
     -p $IP:8302:8302 \
-    -p $IP:8302:8302/udp \     
+    -p $IP:8302:8302/udp \
     -p $IP:8400:8400 \
     -p $IP:8500:8500 \
-    -p $BRIDGE_IP:53 \
+    -p $BRIDGE_IP:53:53 \
     -p $BRIDGE_IP:53:53/udp \
-    --restart=always \
-    progrium/consul \
-    -advertise $IP -join consul.adsabs
+    --restart=always progrium/consul \
+    -advertise $IP \
+    -join consul.adsabs
 
 docker run -d \
     --name logstash-forwarder \
@@ -30,8 +30,8 @@ docker run -d \
     adsabs/logstash-forwarder
 
 docker run -d \
-    --name registrator
+    --name registrator \
     -v /var/run/docker.sock:/tmp/docker.sock \
     --restart=always \
     -h $HOSTNAME \
-    gliderlabs/registrator consul://
+    gliderlabs/registrator consul://$IP:8500
